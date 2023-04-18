489d82972c558809694f5320aee0f955da0d74e1
unknown <maliksachin298@gmail.com>
2023-04-18 17:49:00 +0530

    made fixes

--- palindrome.js
+++ palindrome.js
@@ -4,6 +4,7 @@ let string = "roger";
 for(let index = 0, index2 = string.length-1 ; index <= index2; index++, index2--) {
     if(string[index] != string[index2]) {
         console.log("Not Palindrome");
+        return;
     }
 }
 
